08f40004c67c9dbce97a0d63c6d0b561686f9dc2
Andrew Bancroft <andrewcbancroft@gmail.com>
2015-05-10 14:57:40 -0500

    Added a tableview to display a list of calendars when the user grants permission.

--- EventTracker/ViewController.swift
+++ EventTracker/ViewController.swift
@@ -9,35 +9,90 @@
 import UIKit
 import EventKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     let eventStore = EKEventStore()
     
 	@IBOutlet weak var needPermissionView: UIView!
+	@IBOutlet weak var calendarsTableView: UITableView!
+	
+	var calendars: [EKCalendar]?
 	
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        eventStore.requestAccessToEntityType(EKEntityTypeEvent, completion: {
-            (accessGranted: Bool, error: NSError!) in
-            
-            if accessGranted == true {
-                // take action based on the granted permission
+
+        checkCalendarAuthorizationStatus()
+    }
+	
+	func checkCalendarAuthorizationStatus() {
+		let status = EKEventStore.authorizationStatusForEntityType(EKEntityTypeEvent)
+		
+		switch (status) {
+			case EKAuthorizationStatus.Authorized:
+				loadCalendars()
+				refreshTableView()
+			case EKAuthorizationStatus.NotDetermined:
+				requestAccessToCalendar()
+			case EKAuthorizationStatus.Restricted, EKAuthorizationStatus.Denied:
+				needPermissionView.fadeIn()
+			default:
+				let alert = UIAlertView(title: "Privacy Warning", message: "You have not granted permission for this app to access your Calendar", delegate: nil, cancelButtonTitle: "OK")
+				alert.show()
+		}
+	}
+	
+	func requestAccessToCalendar() {
+		eventStore.requestAccessToEntityType(EKEntityTypeEvent, completion: {
+			(accessGranted: Bool, error: NSError!) in
+			
+			if accessGranted == true {
+				dispatch_async(dispatch_get_main_queue(), {
+					self.loadCalendars()
+					self.refreshTableView()
+				})
 			} else {
-				self.needPermissionView.fadeIn()
+				dispatch_async(dispatch_get_main_queue(), {
+					self.needPermissionView.fadeIn()
+				})
 			}
-        })
-    }
+		})
+	}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+	func loadCalendars() {
+		self.calendars = eventStore.calendarsForEntityType(EKEntityTypeEvent) as? [EKCalendar]
+	}
+	
+	func refreshTableView() {
+		calendarsTableView.hidden = false
+		calendarsTableView.reloadData()
+	}
+	
 	@IBAction func goToSettingsButtonTapped(sender: UIButton) {
 		let openSettingsUrl = NSURL(string: UIApplicationOpenSettingsURLString)
 		UIApplication.sharedApplication().openURL(openSettingsUrl!)
+	}
+	
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		if let calendars = self.calendars {
+			return calendars.count
+		}
+		
+		return 0
+	}
+	
+	
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier("basicCell") as! UITableViewCell
+		
+		if let calendars = self.calendars {
+			let calendarName = calendars[indexPath.row].title
+			cell.textLabel?.text = calendarName
+		} else {
+			cell.textLabel?.text = "Unknown Calendar Name"
+		}
+		
+		return cell
 	}
 }
 
